4b41a258774f202c176f052a5ae5d96653b98577
mahrad <mahradgh@gmail.com>
2021-07-17 09:41:39 +0430

    add bootstrap-add api route-add front page action

--- src/main.py
+++ src/main.py
@@ -1,7 +1,37 @@
 from flask import Flask
+from flask import render_template
+from logging.config import dictConfig
+from flask import request
+from flask import jsonify
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, alireza!</p>"
+
+@app.route('/index')
+def hello():
+    return render_template('index.html')
+
+@app.route('/sentiment',methods=['POST'])
+def sentiment():
+    if request.method == 'POST':
+        response = {"message":request.get_json()['payload']}
+        return jsonify(response)
