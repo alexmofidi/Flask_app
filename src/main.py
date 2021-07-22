e5b314b1e35f0905ed833df5cdcd8f71195cd42c
Alireza Mofidi <amofidi@cudadev03.safetyauthority.ca>
2021-07-21 23:39:00 -0700

    add Classification and Entity

--- src/main.py
+++ src/main.py
@@ -39,8 +39,15 @@ def sentiment():
         document = language_v1.Document(content=request.get_json()['payload'], type_=language_v1.Document.Type.PLAIN_TEXT)
         sentiment = client.analyze_sentiment(request={'document': document}).document_sentiment
         # print("Text: {}".format(text))
-        print("Sentiment: {}, {}".format(sentiment.score, sentiment.magnitude))
-        resp = "Sentiment: " + str(sentiment.score) + ", " + str(sentiment.magnitude)
+        #print("Sentiment: {}, {}".format(sentiment.score, sentiment.magnitude))
+        Sent="Undecided"
+        if sentiment.score > (.1) :
+            Sent="Possitive"
+        elif sentiment.score < (-.1) :
+            Sent="Negative"
+        else:
+            Sent= "Neutral"
+        resp = "Sentiment: {} The score is: ".format(Sent) + str(round(sentiment.score,2)) + ", " + str(round(sentiment.magnitude,2))
         response = {"message":resp}
         return jsonify(response)
 
@@ -56,8 +63,7 @@ def entity():
         response = client.analyze_entities(request = {'document': document, 'encoding_type': encoding_type})
         resp=''
         for entity in response.entities:
-            resp += "Entity: {}".format(entity.name) +"\t" + "Entity type: {}".format(language_v1.Entity.Type(entity.type_).name) + "\n"
-
+            resp += "Entity: {}".format(entity.name) +"--> " + "Entity type: {}".format(language_v1.Entity.Type(entity.type_).name) + "\n"
         print("Entity:  {}".format(resp))
         response = {"message":resp}
         return jsonify(response)
@@ -71,11 +77,29 @@ def classify():
 
         language = "en"
         document = language_v1.Document(content=request.get_json()['payload'], type_=language_v1.Document.Type.PLAIN_TEXT, language= language)
+        response = client.classify_text(request = {'document': document })
+        resp=''
+        for category in response.categories:
+            resp+= u"Category name: {}".format(category.name)  + "--> " + "Confidence: {}".format(round(category.confidence,2))  +  "\n"
+
+        print("Class:  {}".format(resp))
+        response = {"message":resp}
+        return jsonify(response)
+
+@app.route('/summerize',methods=['POST'])
+def summerize():
+    if request.method == 'POST':
+
+        client = language_v1.LanguageServiceClient()
+        type_ = language_v1.Document.Type.PLAIN_TEXT
+
+        language = "en"
+        document = language_v1.Document(content=request.get_json()['payload'], type_=language_v1.Document.Type.PLAIN_TEXT, language= language)
         encoding_type = language_v1.EncodingType.UTF8
         response = client.classify_text(request = {'document': document})
         resp=''
         for category in response.categories:
-            resp+= u"Category name: {}".format(category.name)  + "\t" + "Confidence: {}".format(category.confidence)  + "\n"
+            resp+= u"Category name: {}".format(category.name)  + "    " + "Confidence: {}".format(category.confidence)  + "                       "
 
         print("Class:  {}".format(resp))
         response = {"message":resp}
